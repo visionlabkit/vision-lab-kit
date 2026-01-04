201aae04ada8e47263803b3c1506d5b824497f1f
visionlabkit <visionlabkit@163.com>
2026-01-04 18:02:22 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
+  website: "https://vision-lab-kit.pages.dev/", // replace this with your deployed domain
+  author: "Dr.Z",
   profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  desc: "Ophthalmology Knowledge & Practice Toolkit",
+  title: "Vision LabKit",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
